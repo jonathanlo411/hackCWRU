ccdd940cc606a70005b17486ce44ae9b88ea559e
Jonathan Lo <jonathanlo411@gmail.com>
2022-03-19 10:02:10 -0700

    login, signup, about, dashboard rendered

--- tipship/settings.py
+++ tipship/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'landing',
     'signup',
-    'login'
+    'login',
+    'dashboard'
 ]
 
 MIDDLEWARE = [
--- tipship/urls.py
+++ tipship/urls.py
@@ -3,9 +3,16 @@ from django.contrib import admin
 from django.urls import path
 
 # View Imports
-from landing.views import landing
+from landing.views import landing, about
+from signup.views import signup
+from login.views import login
+from dashboard.views import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', landing, name="landing"),
+    path('about', about, name="about"),
+    path('signup', signup, name="signup"),
+    path('login', login, name="login"),
+    path('dashboard', dashboard, name="dashboard"),
 ]
