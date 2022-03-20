b63e7fa47d970c7d13442f353825293ba51ae8fd
Jonathan Lo <jonathanlo411@gmail.com>
2022-03-20 00:31:51 -0700

    login/signup utilizes fields

--- signup/models.py
+++ signup/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 class userprofile(models.Model):
     # User Information
-    did = models.CharField(max_length=100, default="error")
     username = models.CharField(max_length=30, default="error")
+    password = models.CharField(max_length=50, default="error")
+    did = models.CharField(max_length=100, default="error")
     access_token = models.CharField(max_length=100, default="error")
     refresh_token = models.CharField(max_length=100, default="error")
 
