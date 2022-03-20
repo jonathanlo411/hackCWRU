80deb31476e43e2feadf0470239acfad75078eaa
leeb5737 <brl013@ucsd.edu>
2022-03-20 00:37:36 -0700

    push before test

--- signup/models.py
+++ signup/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 class userprofile(models.Model):
     # User Information
-    username = models.CharField(max_length=30, default="error")
-    password = models.CharField(max_length=50, default="error")
     did = models.CharField(max_length=100, default="error")
+    username = models.CharField(max_length=30, default="error")
     access_token = models.CharField(max_length=100, default="error")
     refresh_token = models.CharField(max_length=100, default="error")
 
